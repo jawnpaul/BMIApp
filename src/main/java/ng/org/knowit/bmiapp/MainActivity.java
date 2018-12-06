54b39b7ac2b34a7e9d0f4ea162d47635aee66c06
John Paul <jpaul5246@gmail.com>
2018-12-06 11:35:54 +0100

    Finished up the UI of the MainActivity

--- src/main/java/ng/org/knowit/bmiapp/MainActivity.java
+++ src/main/java/ng/org/knowit/bmiapp/MainActivity.java
@@ -2,12 +2,30 @@ package ng.org.knowit.bmiapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    double height, weight, bmiResult;
+
+    TextView heightTextView, weightTextView, bmiResultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar heightSeekBar = findViewById(R.id.heightSeekBar);
+
+        bmiResultTextView = findViewById(R.id.bmiTextView);
+
+
+    }
+
+    private void calculateBmi(double weight, double height) {
+
+        bmiResult = weight / Math.pow(height, 2);
+        bmiResultTextView.setText(String.valueOf(bmiResult));
     }
 }
