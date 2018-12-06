bc5ad7ad59c4ff441f55df3944af713b5259da0b
John Paul <jpaul5246@gmail.com>
2018-12-06 12:20:09 +0100

    Implemented the logic for calculating BMI

--- src/main/java/ng/org/knowit/bmiapp/MainActivity.java
+++ src/main/java/ng/org/knowit/bmiapp/MainActivity.java
@@ -10,22 +10,71 @@ public class MainActivity extends AppCompatActivity {
     double height, weight, bmiResult;
 
     TextView heightTextView, weightTextView, bmiResultTextView;
+    SeekBar heightSeekBar, weightSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SeekBar heightSeekBar = findViewById(R.id.heightSeekBar);
+       heightSeekBar = findViewById(R.id.heightSeekBar);
+       weightSeekBar = findViewById(R.id.weightSeekBar);
+       bmiResultTextView = findViewById(R.id.bmiTextView);
 
-        bmiResultTextView = findViewById(R.id.bmiTextView);
+       heightTextView = findViewById(R.id.heightTextView);
+       weightTextView = findViewById(R.id.weightTextView);
 
+       heightSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+           @Override
+           public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+               height = seekBar.getProgress();
+               heightTextView.setText(String.valueOf(height));
+               bmiResultTextView.setText(String.valueOf(calculateBmi(weight, height)));
+           }
+
+           @Override
+           public void onStartTrackingTouch(SeekBar seekBar) {
+               height = seekBar.getProgress();
+               heightTextView.setText(String.valueOf(height));
+               bmiResultTextView.setText(String.valueOf(calculateBmi(weight, height)));
+           }
+
+           @Override
+           public void onStopTrackingTouch(SeekBar seekBar) {
+               height = seekBar.getProgress();
+               heightTextView.setText(String.valueOf(height));
+               bmiResultTextView.setText(String.valueOf(calculateBmi(weight, height)));
+           }
+       });
+
+       weightSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+           @Override
+           public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+               weight = seekBar.getProgress();
+               weightTextView.setText(String.valueOf(weight));
+               bmiResultTextView.setText(String.valueOf(calculateBmi(weight, height)));
+           }
+
+           @Override
+           public void onStartTrackingTouch(SeekBar seekBar) {
+               weight = seekBar.getProgress();
+               weightTextView.setText(String.valueOf(weight));
+               bmiResultTextView.setText(String.valueOf(calculateBmi(weight, height)));
+           }
+
+           @Override
+           public void onStopTrackingTouch(SeekBar seekBar) {
+               weight = seekBar.getProgress();
+               weightTextView.setText(String.valueOf(weight));
+               bmiResultTextView.setText(String.valueOf(calculateBmi(weight, height)));
+           }
+       });
 
     }
 
-    private void calculateBmi(double weight, double height) {
-
+    private double calculateBmi(double weight, double height) {
         bmiResult = weight / Math.pow(height, 2);
-        bmiResultTextView.setText(String.valueOf(bmiResult));
+        return bmiResult;
     }
+
 }
